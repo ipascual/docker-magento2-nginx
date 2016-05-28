4d28e4d4b24cdb20e5f8a9da2a38ac63d56ff27e
Ignacio Pascual <ipascual@unexpectedit.com>
2016-05-28 11:44:05 -0500

    User NFS changes

--- bin/start.sh
+++ bin/start.sh
@@ -4,4 +4,7 @@
 [ ! -z "${APP_MAGE_MODE}" ]            && sed -i "s/APP_MAGE_MODE/${APP_MAGE_MODE}/" /etc/nginx/conf.d/default.conf
 [ ! -z "${WWW_DIR}" ]                  && sed -i "s~WWW_DIR~${WWW_DIR}~" /etc/nginx/conf.d/default.conf
 
+# NFS permissions
+[ ! -z "${USER_UID}" ]         && usermod -u $USER_UID www-data
+
 /usr/sbin/nginx -g "daemon off;"
